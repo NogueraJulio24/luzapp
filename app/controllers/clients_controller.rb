97c48f5b2f426984dfec3519b8cf1775748bf29b
Julio Noguera <noguerajulio24@gmail.com>
2018-01-15 15:26:47 -0300

    client search

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -4,7 +4,7 @@ class ClientsController < ApplicationController
   # GET /clients
   # GET /clients.json
   def index
-    @clients = Client.all
+    @clients = Client.where('name LIKE ?', "%#{params[:search]}%")
   end
 
   # GET /clients/1
